396a8bd16d992d7d940b3bd042fcaecfd7ab2d6c
Ashish Kathuria <xs2ashish@gmail.com>
2018-11-19 11:08:40 +0530

    v0.2

--- py_examples/play_with_strings.py
+++ py_examples/play_with_strings.py
@@ -2,6 +2,7 @@ def str_func(str1):
     try:
         print(str1)
         print(str1[0])  # First character of string
+        print(str1[2])  # Third character of string
         print(str1[0:2])  # String starting at position 0 with 2 (2-0) characters
         print(str1[5:11])  # String starting at position 5(6th) with 6 (11-5) characters
         print(len(str1))  # Length of string
@@ -11,6 +12,7 @@ def str_func(str1):
         print(str1.split(" "))  # Split into substrings returning list of values
         print(str1 + " " + str1)  # Concatenate strings
         print(str1.capitalize())  # First Upper of string
+        print(str1.split(" ")[0] + " " + str1.split(" ")[3])  # AWK alternate
     except:
         print("Error during string function")
 
