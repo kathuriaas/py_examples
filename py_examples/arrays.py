5dfbec1346f9ecd3058723667491585614d6dd78
Ashish Kathuria <xs2ashish@gmail.com>
2018-11-19 12:24:29 +0530

    v0.3

--- py_examples/arrays.py
+++ py_examples/arrays.py
@@ -1,4 +1,5 @@
 def array_list(p_list):
+    print("List example started:")
     try:
         for element in p_list:
             print(element)
@@ -18,14 +19,42 @@ def array_list(p_list):
         print(p_list)
     except:
         print("Error during list")
+    print("List example Ended\n")
 
 
 def array_tuple(p_tuple):
+    print("Tuple example started:")
     try:
         for element in p_tuple:
             print(element)
+        print(p_tuple[2:])
+        if 'element1' in p_tuple:
+            print('element1', ' is present in ', p_tuple)
+        print(p_tuple.count('element1'))  # Count of element1 in tuple
     except:
         print("Error during list")
+
+    try:
+        p_tuple[0] = 'New_element'  # This will raise error
+        print(p_tuple)
+    except:
+        print('Error during tuple change--tuples are immutable')
+
+    try:
+        del p_tuple  # Delete this tuple completely
+        print(p_tuple)
+    except:
+        print('Tuple deleted')
+    print("Tuple example Ended\n")
+
+
+def array_set(p_set):
+    print("Set example started:")
+    try:
+        print(p_set)
+    except:
+        print("Error during set")
+    print("Set example Ended\n")
 
 
 def main():
@@ -33,6 +62,8 @@ def main():
     array_list(v_list)
     v_tuple = ('element1', 'element2', 'element3', 'element4')
     array_tuple(v_tuple)
+    v_set = {'set_element1', 'set_element2', 'set_element3', 'set_element4'}
+    array_set(v_set)
 
 
 if __name__ == "__main__":
